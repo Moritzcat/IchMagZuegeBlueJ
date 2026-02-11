a49901848310fa7433305b0ef7ed1e37fa9f547a
Schul-PC-J <jakobee@gmx.de>
2026-02-11 08:57:01 +0100

    Initial sharing of project

---
+++ ConnectionPoint.java
@@ -0,0 +1,16 @@
+
+/**
+ * Point where two Tracks connect
+ * 
+ * @author JakobThomasGames
+ * @version 11.02.26
+ */
+public class ConnectionPoint extends DirPoint
+{
+    
+    public ConnectionPoint(double x,double y, double angle)
+    {
+       super( x, y, angle); 
+    }
+
+}
