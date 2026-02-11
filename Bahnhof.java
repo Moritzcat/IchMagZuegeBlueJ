59ae89e32611c71bd8622d3586c5bd67fabeec3f
Schul-PC-J <jakobee@gmx.de>
2026-02-11 09:20:44 +0100

    Test

--- Bahnhof.java
+++ Bahnhof.java
@@ -5,29 +5,11 @@
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
-public class Bahnhof
+public class Bahnhof extends Track
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
 
-    /**
-     * Konstruktor für Objekte der Klasse Bahnhof
-     */
     public Bahnhof()
     {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+        
     }
 }
---
+++ GameUI.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Beschreiben Sie hier die Klasse GameUI.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class GameUI extends JPanel
+{
+    
+    public GameUI()
+    {
+        
+    }
+
+}
